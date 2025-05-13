c85fded8648f18f0a960c2932079439b02c4c1e8
Matthew Burgess <boog397@gmail.com>
2025-05-12 21:41:45 -0500

    Add useShopItems hook to retrieve shop items from the internet

---
+++ src/hooks/useShopItems.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export default function useShopItems() {
+  const [shopItems, setShopItems] = useState([]);
+  const [shopItemsIsLoading, setShopItemsIsLoading] = useState(true);
+  const [shopItemsError, setShopItemsError] = useState("");
+
+  useEffect(() => {
+    async function getShopItems() {
+      setShopItems([]);
+      setShopItemsIsLoading(true);
+      setShopItemsError("");
+
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (response.ok) {
+          const json = await response.json();
+          setShopItems(json);
+        } else {
+          throw new Error("Response was not ok");
+        }
+      } catch (error) {
+        console.log(error);
+        setShopItemsError("Unable to retrieve shop items.");
+      } finally {
+        setShopItemsIsLoading(false);
+      }
+    }
+
+    getShopItems();
+  }, []);
+
+  return { shopItems, shopItemsIsLoading, shopItemsError };
+}

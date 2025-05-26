abe05360177cdca3def362dca50c1b8de1c6d8d3
Matthew Burgess <boog397@gmail.com>
2025-05-26 13:20:59 -0500

    Add preliminary content to CartSideMenu

--- src/components/menu/CartSideMenu.jsx
+++ src/components/menu/CartSideMenu.jsx
@@ -1,9 +1,26 @@
 import "../../styles/CartSideMenu.css";
 
-export default function CartSideMenu({ isOpen, setIsOpen }) {
+export default function CartSideMenu({ isOpen, setIsOpen, cartItems }) {
   return (
-    <div className={`cart-side-menu ${isOpen ? "open" : ""}`}>
-      <button onClick={() => setIsOpen(!isOpen)}>Close</button>
-    </div>
+    <section className={`cart-side-menu ${isOpen ? "open" : ""}`}>
+      <div className="header">
+        <h2 className="title">Shopping Cart - 1 Item(s)</h2>
+        <button onClick={() => setIsOpen(!isOpen)}>Close</button>
+      </div>
+      <div className="body">
+        {cartItems === undefined ||
+        cartItems === null ||
+        cartItems.length === 0 ? (
+          <div className="empty-cart-message">
+            <span>Looks like your cart is empty.</span>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="footer">
+        <button>Proceed to Checkout</button>
+      </div>
+    </section>
   );
 }

2262851e09f8c385cf67ca1856e39e3ce8a119c7
Matthew Burgess <boog397@gmail.com>
2025-05-25 23:15:56 -0500

    Add quantity selector to ShopItemCard and adjust ShopItemCard footer styles

--- src/components/card/ShopItemCard.jsx
+++ src/components/card/ShopItemCard.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import "../../styles/ShopItemCard.css";
 import NumberUtils from "../../utils/NumberUtils";
 
 export default function ShopItemCard({ title, price, description, imageUrl }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function onQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
+
   // TODO: Revise as needed
   return (
     <div className="shop-item-card">
@@ -10,6 +17,17 @@ export default function ShopItemCard({ title, price, description, imageUrl }) {
       <div className="description">{description}</div>
       <div className="footer">
         <div className="price">{NumberUtils.formatAsDollars(price)}</div>
+        <label htmlFor="quantity">
+          <span>Quantity</span>
+          <input
+            id="quantity"
+            className="quantity"
+            type="number"
+            value={quantity}
+            onChange={onQuantityChange}
+          />
+        </label>
+
         <button>Add to Cart</button>
       </div>
     </div>

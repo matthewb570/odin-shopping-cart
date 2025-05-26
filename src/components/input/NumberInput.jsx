f81fe6ef2d511c1133971227e42b4e11cbf05d9e
Matthew Burgess <boog397@gmail.com>
2025-05-26 15:11:15 -0500

    Disable negative numbers by default in NumberInput

--- src/components/input/NumberInput.jsx
+++ src/components/input/NumberInput.jsx
@@ -1,17 +1,38 @@
 import Icon from "../icon/Icon";
 import "../../styles/NumberInput.css";
 
-export default function NumberInput({ value, setValue }) {
+export default function NumberInput({
+  value,
+  setValue,
+  allowNegatives = false,
+}) {
+  function handleInputChange(event) {
+    const intValue = parseInt(event.target.value);
+
+    if (allowNegatives) {
+      setValue(intValue || 0);
+    } else {
+      setValue(intValue >= 0 ? intValue : 0);
+    }
+  }
+
   return (
     <div className="number-input">
       <button
         className="icon-button secondary"
         aria-label="Decrease quantity"
         onClick={() => setValue(value - 1)}
+        disabled={!allowNegatives && value <= 0}
       >
         <Icon iconName={"minus"} />
       </button>
-      <input type="number" aria-label="Quantity" value={value} />
+      <input
+        type="number"
+        aria-label="Quantity"
+        value={value.toString()}
+        onChange={handleInputChange}
+        min={0}
+      />
       <button
         className="icon-button secondary"
         aria-label="Increase quantity"
